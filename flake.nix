0bf893dfc6c8d2ab1aa5a6c68bfe3d6a6c787488
Peeters Tijn T.R. <i547988@fontysict.nl>
2025-11-20 13:43:10 +0100

    update nix flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,16 @@
 {
-  description = "A Nix-flake-based Kotlin + Gradle + Android dev environment with GraalVM";
+  description = "A Nix-flake-based Kotlin + Gradle + Android dev environment with OpenJDK";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     android-nixpkgs.url = "github:tadfisher/android-nixpkgs";
+    gradle2nix.url = "github:tadfisher/gradle2nix/v2";
   };
 
-  outputs = { self, nixpkgs, android-nixpkgs }: let
+  outputs = { self, nixpkgs, android-nixpkgs, gradle2nix }: let
     supportedSystems = [
       "x86_64-linux"
       "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
     ];
 
     forEachSupportedSystem = f:
@@ -19,83 +18,88 @@
         f {
           pkgs = import nixpkgs {
             inherit system;
-            config = {
-              allowUnfree = true;
-            };
+            config = { allowUnfree = true; };
           };
         }
       );
   in
   {
-	packages = forEachSupportedSystem ({ pkgs }: {
+    packages = forEachSupportedSystem ({ pkgs }: 
+    {
       idea-launcher = pkgs.writeShellScriptBin "idea" ''
         exec ${pkgs.jetbrains.idea-ultimate}/bin/idea-ultimate \
           -Dawt.toolkit.name=WLToolkit "$@" \
           >/dev/null 2>&1 &
       '';
+
+      jdk = pkgs.javaPackages.compiler.openjdk21;
     });
 
-    devShells = forEachSupportedSystem ({ pkgs }:
-      {
-        default =
-        let
-          androidSdk = android-nixpkgs.sdk.${pkgs.system} (sdkPkgs: with sdkPkgs; [
-            cmdline-tools-latest
-            build-tools-36-0-0
-            platform-tools
-            platforms-android-36
-            emulator
-          ]);
-        in
-        pkgs.mkShell {
-          packages = with pkgs; [
-            gcc
-            gradle_9
-            kotlin
-            jdk24
-            ncurses
-            patchelf
-            zlib
-            libGL
-            xorg.libX11
-			jetbrains.idea-ultimate
-			self.packages.${pkgs.system}.idea-launcher
+    packages.x86_64-linux.melay-app = gradle2nix.builders.x86_64-linux.buildGradlePackage {
+      pname = "melay-app";
+      version = "1.0.0";
+      lockFile = ./gradle.lock;
+      gradleInstallFlags = [ ":app:packageReleaseUberJarForCurrentOS" ];
+    };
 
-            # Android SDK + NDK
-            androidSdk
-          ];
+    devShells = forEachSupportedSystem ({ pkgs }: let
+      androidSdk = android-nixpkgs.sdk.${pkgs.system} (sdkPkgs: with sdkPkgs; [
+        cmdline-tools-latest
+        build-tools-36-0-0
+        platform-tools
+        platforms-android-36
+        emulator
+      ]);
+    in
+    {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          gcc
+          gradle_9
+          kotlin
+          self.packages.${pkgs.system}.jdk
+          ncurses
+          patchelf
+          zlib
+          libGL
+          xorg.libX11
+          jetbrains.idea-ultimate
+          self.packages.${pkgs.system}.idea-launcher
 
-          JAVA_TOOL_OPTIONS = "--enable-native-access=ALL-UNNAMED";
-          SPRING_PROFILES_ACTIVE = "dev";
+          # Android SDK + NDK
+          androidSdk
+        ];
 
-          shellHook = ''
-            export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${
-              pkgs.lib.makeLibraryPath [
-                pkgs.libGL
-                pkgs.xorg.libX11
-              ]
-            }
+        JAVA_TOOL_OPTIONS = "--enable-native-access=ALL-UNNAMED";
+        SPRING_PROFILES_ACTIVE = "dev";
 
-            # Java from Nix
-            export JAVA_HOME=${pkgs.jdk24}/lib/openjdk
-            export PATH=$JAVA_HOME/bin:$PATH
+        shellHook = ''
+          export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${
+            pkgs.lib.makeLibraryPath [
+              pkgs.libGL
+              pkgs.xorg.libX11
+            ]
+          }
 
-            # Gradle from Nix
-            export GRADLE_HOME=${pkgs.gradle_9}/lib/gradle
-            export PATH=$GRADLE_HOME/bin:$PATH
+          # Java from Nix
+          export JAVA_HOME=${self.packages.${pkgs.system}.jdk}/lib/openjdk
+          export PATH=$JAVA_HOME/bin:$PATH
 
-            # Android Home
-            export ANDROID_HOME=${androidSdk}/share/android-sdk
-            export PATH=ANDROID_HOME/bin:$PATH
+          # Gradle from Nix
+          export GRADLE_HOME=${pkgs.gradle_9}/lib/gradle
+          export PATH=$GRADLE_HOME/bin:$PATH
 
-            # Kotlin (if installed separately)
-            export PATH=${pkgs.kotlin}/bin:$PATH
+          # Android Home
+          export ANDROID_HOME=${androidSdk}/share/android-sdk
+          export PATH=$ANDROID_HOME/bin:$PATH
 
-            # Optional: force IntelliJ to pick up the local Gradle instead of wrapper
-            export ORG_GRADLE_PROJECT_gradleUserHome=$GRADLE_HOME
-          '';
-        };
-      }
-    );
+          # Kotlin (if installed separately)
+          export PATH=${pkgs.kotlin}/bin:$PATH
+
+          # Optional: force IntelliJ to pick up the local Gradle instead of wrapper
+          export ORG_GRADLE_PROJECT_gradleUserHome=$GRADLE_HOME
+        '';
+      };
+    });
   };
 }
