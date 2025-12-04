9ac15c55d1a236d086991426329353cadc445102
Peeters,Tijn T.R. <i547988@fontysict.nl>
2025-12-04 16:02:40 +0100

    generate documentation with structurizr

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,9 +30,10 @@ dependencyResolutionManagement {
 }
 
 plugins {
-	id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 include("server")
 include("app")
 include("common")
+include("architecture")
